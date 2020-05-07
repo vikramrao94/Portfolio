d204ac5a2271cd3cacf815823beeb7a2bbb81ade
Vikram Rao <vikramrao2512@gmail.com>
2020-05-07 13:13:23 -0400

    Added pdf creator backbone for backend

---
+++ portfolio-backend/main.py
@@ -0,0 +1,6 @@
+from cv_creator import CV_Creator
+from resume_creator import Resume_Creator
+
+
+resume = Resume_Creator("resume");
+resume.add_header("blah");
---
+++ portfolio-backend/resume_creator.py
@@ -0,0 +1,27 @@
+
+class Resume_Creator:
+
+    def __init__(self, file_name):
+        self.file_name = file_name
+        print("filename: ",file_name)
+
+    def add_summary(self, data):
+        print("summary")
+
+    def add_experience(self, data):
+        print("experience")
+
+    def add_skills(self, data):
+        print("skills")
+
+    def add_education(self, data):
+        print("education")
+
+    def add_header(self, data):
+        print("header")
+
+    def add_projects(self, data):
+        print("projects")
+
+    def save_resume(self, data):
+        print("save resume")
