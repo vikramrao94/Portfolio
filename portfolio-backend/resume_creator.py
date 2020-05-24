1f2681a8c361331f879d2e0b7f09c0b9a10b171f
Vikram Rao <vikramrao2512@gmail.com>
2020-05-24 09:13:44 -0400

    Added education details to resume creator.

--- portfolio-backend/resume_creator.py
+++ portfolio-backend/resume_creator.py
@@ -1,8 +1,10 @@
 import json
 from io import BytesIO
+from reportlab.lib import colors
 from reportlab.lib.pagesizes import letter, A4
 from reportlab.platypus import SimpleDocTemplate, Paragraph, PageBreak, Spacer, Table
-from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib.enums import TA_JUSTIFY,TA_LEFT,TA_CENTER,TA_RIGHT
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from reportlab.lib.units import inch, mm
 from reportlab.lib.colors import HexColor
 from PIL import Image
@@ -44,6 +46,8 @@ class Resume_Creator:
                 body_style.fontSize = parameters[key]
             if key == "textColor":
                 body_style.textColor = parameters[key]
+            if key == "alignment":
+                body_style.alignment = parameters[key]
         if type == "Bullet":
             body_style.leftIndent = self.bullet_properties["indent"];
         return body_style;
@@ -54,6 +58,13 @@ class Resume_Creator:
         });
         for point in array:
             self.data.append(Paragraph(point, body_style, bulletText = self.bullet_properties["symbol"]));
+
+    def generate_range(self, date):
+        style = ParagraphStyle('Normal',
+                    alignment = TA_RIGHT,
+                    fontSize = 10.5
+                    )
+        return Paragraph(date, style)
 
     def generate_heading(self, title):
         body_style = self.get_body_style("Normal",{
@@ -82,24 +93,101 @@ class Resume_Creator:
         print("skills")
 
     def add_education(self):
+        self.generate_heading("<b>EDUCATION</b>")
+        for education in self.input["education"]:
+            print(education)
+            word_style = self.get_body_style("Normal", {
+            "fontSize":10.5,
+            })
+            print(TA_RIGHT)
+            duration_style = self.get_body_style("Normal", {
+            "fontSize":10.5,
+            "alignment":"right"
+            })
+            logo = [
+                [
+                HyperlinkedImage("images/" + education["logo"],None, 20, 25)
+                ]
+            ]
+            degree = [[
+                Paragraph("<b>%s</b>"% education["degree"],word_style),
+                Paragraph("<i>%s</i>"%education["course"],word_style)
+            ]]
+            school = [[
+                Paragraph(education["school"],word_style),
+                Paragraph(education["location"],word_style)
+            ]]
+
+            educate = [
+                [Table(degree, [30, 300])],
+                [Table(school, [150, 180])]
+            ]
+            styles = ParagraphStyle('Normal',
+                            alignment = TA_RIGHT,
+                            fontSize = 10.5,
+                            fontName="Times-Roman")
+            duration = [
+                [
+                    self.generate_range(education["duration"])
+                ]
+            ]
+            logo_table = Table(logo)
+
+            education_table = Table(educate)
+            duration_table = Table(duration)
+            main = [
+                [
+                logo_table,
+                education_table,
+                duration_table
+                ]
+            ]
+            main_table = Table(main, [30,330,200])
+            main_table_style = [
+                ('VALIGN', (0, 0), (-1, -1), "MIDDLE"),
+            ]
+            main_table.setStyle(main_table_style)
+            self.data.append(main_table)
         print("education")
 
     def add_header(self):
-        print(self.input["heading"])
+        # print(self.input["heading"])
         # img = Image.open('images/linkedin.png')
         name_style = self.get_body_style("Normal",{
         "fontSize": 24,
         "textColor": HexColor("#ff8100")
         });
-        linkedin = HyperlinkedImage('images/linkedin.png', self.input["heading"]["linkedin"],30,30)
-        github = HyperlinkedImage('images/github.png', self.input["heading"]["github"],30,30)
-        table_data = [
+        sub_heading_style = self.get_body_style("Normal", {
+        "fontSize": 10.5
+        })
+        linkedin = HyperlinkedImage('images/linkedin.png', self.input["heading"]["linkedin"],25,20)
+        github = HyperlinkedImage('images/github.png', self.input["heading"]["github"],20,20)
+        personal_website = HyperlinkedImage('images/me.png', self.input["heading"]["website"],20,20)
+        phone_image = HyperlinkedImage('images/phone.png', None, 20,20)
+        address_image = HyperlinkedImage('images/address.png', None, 20, 20)
+        heading = [
             [Paragraph("<b>%s</b>" % self.input["heading"]["name"], name_style)],
-            [Spacer(1, 10)],
-            [github, linkedin]
         ]
-        table = Table(table_data)
-        self.data.append(table)
+        heading_table = Table(heading)
+        heading_table_style = [
+            ('VALIGN', (0, 0), (-1, -1), 'MIDDLE')
+        ]
+        heading_table.setStyle(heading_table_style)
+        self.data.append(heading_table)
+        self.data.append(Spacer(1, 20))
+        phone = Paragraph(self.input["heading"]["phone"], sub_heading_style)
+        address = Paragraph(self.input["heading"]["address"],sub_heading_style)
+        sub_heading = [
+        [phone_image ,phone ,address_image ,address, personal_website,linkedin, github]
+        ]
+        sub_heading_table_style = [
+            ('VALIGN', (0, 0), (-1, -1), 'MIDDLE'),
+            ('ALIGN', (1, 1), (1, -1), 'LEFT')
+        ]
+        sub_heading_table = Table(sub_heading,[25,100,25,312,25,25,25])
+        sub_heading_table.setStyle(sub_heading_table_style)
+        self.data.append(sub_heading_table)
+        self.data.append(Spacer(1, 10))
         print("header")
 
     def add_projects(self):
@@ -108,8 +196,9 @@ class Resume_Creator:
     def save_resume(self):
         self.add_header();
         self.add_summary();
+        self.add_education();
         # self.add_skills();
-        self.add_experience();
+        # self.add_experience();
         self.doc.build(self.data);
         with open(self.file_name + ".pdf", "wb") as f:
             f.write(self.pdf_buffer.getbuffer())
